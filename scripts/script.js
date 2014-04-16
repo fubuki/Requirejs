0a5a2ddbc88ba2a8a97b50c97e4d91136f02e69f
fubuki <fubuki@users.noreply.github.com>
2014-04-16 22:23:56 +0800

    測試D3 bar chart的語法。

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
  
 var container = d3.select('body')
   .append('svg')
+  .attr('class', 'block')
   .attr('width', width + margin.left + margin.right)
   .attr('height', height + margin.top + margin.bottom);
  
@@ -42,7 +43,6 @@ svg.append('g')
   .attr('class', 'x axis')
   .attr('transform', 'translate(0,' + height + ')')
   .call(xAxis)
-
   .append('text')
   .text('token')
   .attr('transform', 'translate(' + width + ', 0)');
@@ -52,3 +52,55 @@ svg.append('g')
   .call(yAxis)
   .append('text')
   .text('count');
+
+
+
+var dataset = [ 25, 7, 5, 26, 11, 8, 25, 14, 23, 19,
+14, 11, 22, 29, 11, 13, 12, 17, 18, 10,
+24, 18, 25, 9, 3 ];
+
+d3.select("body").selectAll("div")
+	.data(dataset)  // <-- The answer is here!
+	.enter()
+	.append("div")
+	.attr("class", "bar")
+	.style("height", function(d) {
+	var barHeight = d * 5;
+	return barHeight + "px";
+});
+
+var w = 500;
+var h = 100;
+var barPadding = 1;
+
+var svg = d3.select("body")
+.append("svg")
+.attr("width", w)
+.attr("height", h);
+
+svg.selectAll("rect")
+.data(dataset)
+.enter()
+.append("rect")
+.attr({
+x: function(d, i) { return i * (w / dataset.length); },
+y: function(d) { return h - (d * 4); },
+width: w / dataset.length - barPadding,
+height: function(d) { return d * 4; },
+fill: function(d) { return "rgb(0, 0, " + (d * 10) + ")"; }
+});
+
+
+function draw(data) {
+	d3.select("body")
+	.append("div")
+	.attr("class","chart")
+	.selectAll(".bar")
+	.data(data.cash)
+	.enter()
+	.append("div")
+	.attr("class","bar")
+	.style("width", function(d){return d.count/100 + "px"})
+	.style("outline", "1px solid black")
+	.text(function(d){return Math.round(d.count)});
+}
